9da640ca63162a7f1678646f71175f677f5df86b
javierarana115 <javierarana115@gmail.com>
2024-03-23 19:29:30 -0400

    Text Box Added

--- src/App.jsx
+++ src/App.jsx
@@ -10,22 +10,11 @@ function App() {
       <div className="header-container">
         <div className="logo">
           <a href="./App.jsx" target="_blank">
-            <img className="picture" src={sustainabilityImage} alt="Hand Logo" height={300} />
+            <img className="picture" src={sustainabilityImage} alt="Hand Logo"/>
           </a>
         </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input type='check box' className='chat-box' height={200} ></ input>
     </>
   );
 }

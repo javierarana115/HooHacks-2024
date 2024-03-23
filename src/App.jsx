d3449986d6af510a4c5632752af42f085a92813d
Miles1744 <Mithomtab@gmail.com>
2024-03-23 18:29:07 -0400

    Changed styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import sustainabilityImage from './assets/sustainability1721.jpg'; 
-import './App.css'
+import React, { useState } from 'react';
+import sustainabilityImage from './assets/sustainable-green-svgrepo-com.svg'; 
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src="src\assets\sustainability1721.jpg" alt="Hand Logo" />
-        </a>
+      <div className="header-container">
+        <div className="logo">
+          <a href="./App.jsx" target="_blank">
+            <img className="picture" src={sustainabilityImage} alt="Hand Logo" height={300} />
+          </a>
+        </div>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount(count + 1)}>
           count is {count}
         </button>
         <p>
@@ -30,7 +27,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

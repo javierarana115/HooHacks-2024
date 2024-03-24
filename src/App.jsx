eaddcf29de3e98dd9a4a51e5915ab0f6bd98e8f5
PotatoCodo <aquach1990@gmail.com>
2024-03-24 02:19:21 -0400

    Edited css

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,26 @@ function App() {
           <a href="./App.jsx" target="_blank">
             <img className="picture" src={sustainabilityImage} alt="Hand Logo"/>
           </a>
+          <h1 id="main-heading">Hoohacks 2024 Project</h1>
         </div>
       </div>
-      <input type='check box' className='chat-box' height={200} ></ input>
+      
+      <div className="buttons-container">
+        <button className="button">Button 1</button>
+        <button className="button">Button 2</button>
+        <button className="button">Button 3</button>
+        <button className="button">Button 4</button>
+        </div>
+        <div>
+        <button className="button">Button 5</button>
+        <button className="button">Button 6</button>
+      </div>
+      <br></br><br></br>
+      <div>
+      <input type='check box' className='chat-box' height={100} ></ input>
+      </div>
     </>
+    
   );
 }
 
